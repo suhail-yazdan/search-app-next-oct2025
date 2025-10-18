5a299b7f4d6f4d65d062051ff68c2adf2372d43b
unknown <Administrator@blrtraining1.in>
2025-10-18 18:48:11 +0530

    page.js completed Card and Widget components initiated

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,8 @@ export default function Home() {
   
   const [selectedCategory, setSelectedCategory] = useState('');
 
+  const [searchQuery, setSearchQuery] = useState('');
+
   const [results, setResults] = useState([]);
   const [category, setCategory] = useState([]);
  
@@ -23,14 +25,19 @@ export default function Home() {
           ...response.data.Buses.map(item => ({ ...item, serviceType: "Buses" })),
           ...response.data.Flights.map(item => ({ ...item, serviceType: "Flights" })),
           ...response.data.Hotels.map(item => ({ ...item, serviceType: "Hotels" }))
-        ]
-        
-        updatedResults = allData
+        ];
 
+        allData.forEach((elem) => {
+          if(elem.serviceName.toLowerCase().includes(searchQuery.toLowerCase())){
+            updatedResults.push(elem)
+          }
+        })
       } else {
-        let bmtServices = response.data[category]
+        const bmtServices = response.data[category];
         bmtServices.forEach((elem) => {
-          updatedResults.push(elem)
+          if (elem.serviceName.toLowerCase().includes(searchQuery.toLowerCase())) {
+            updatedResults.push(elem)
+          }
         })
       }
 
@@ -66,6 +73,11 @@ export default function Home() {
     }
   }, [])
 
+  const handleSubmit = () => {  
+    setResults([]);
+    makeAjaxCall(selectedCategory);
+  };
+
   return (
     <>
       <header className="h-[320px] bg-[url(../../public/images/clouds.jpg)] bg-center bg-cover border-t">
@@ -75,9 +87,11 @@ export default function Home() {
               <input 
                   id="search"
                   type="text" 
+                  value = {searchQuery}
                   placeholder='Search for the name of a bus service, flight service or a hotel'
                   autoComplete='off'
                   className='w-full h-[40px] rounded-l-3xl shadow-lg ps-5 bg-white'
+                  onChange={(e) => setSearchQuery(e.target.value)}
                   onClick={() => setDropdownVisibility(!dropdownVisibility)}
               />
 
@@ -85,28 +99,28 @@ export default function Home() {
                   type="button"
                   className='h-[40px] px-20 rounded-r-3xl cursor-pointer bg-gray-200 hover:bg-gray-300 border-l border-gray-400'
                   value="Submit"
-                  onClick={() => makeAjaxCall(selectedCategory)}
+                  onClick={handleSubmit}
               />
             </div>
 
             {dropdownVisibility && <section className="bg-white w-[81%] ml-[1.7%] flex items-center py-5 px-10 ">
               <div className='flex items-center mr-4'>
-                <input type="radio" name="category" value="Buses" onChange={(e) => setSelectedCategory(e.target.value)} />
+                <input type="radio" name="category" value="Buses" onChange={(e) => {setSelectedCategory(e.target.value); setSearchQuery('')}} />
                 <label className="grid items-center grid-cols-2 gap-3 ml-2">Buses <FaBusSimple /> </label>
               </div>
 
               <div className='flex items-center mr-4'>
-                <input type="radio" name="category" value="Flights" onChange={(e) => setSelectedCategory(e.target.value)} />
+                <input type="radio" name="category" value="Flights" onChange={(e) => {setSelectedCategory(e.target.value); setSearchQuery('')}} />
                 <label className="grid items-center grid-cols-2 gap-3 ml-2">Flights <FaPlaneUp /> </label>
               </div>
 
               <div className='flex items-center mr-4'>
-                <input type="radio" name="category" value="Hotels" onChange={(e) => setSelectedCategory(e.target.value)} />
+                <input type="radio" name="category" value="Hotels" onChange={(e) => {setSelectedCategory(e.target.value); setSearchQuery('')}} />
                 <label className="grid items-center grid-cols-2 gap-3 ml-2">Hotels <FaHotel /> </label>
               </div>
 
               <div className='flex items-center mr-4'>
-                <input type="radio" name="category" value="All" onChange={(e) => setSelectedCategory(e.target.value)} />
+                <input type="radio" name="category" value="All" onChange={(e) => {setSelectedCategory(e.target.value); setSearchQuery('')}} />
                 <label className="grid items-center grid-cols-2 gap-3 ml-2">All <FaChessBoard /> </label>
               </div>
             </section>}
